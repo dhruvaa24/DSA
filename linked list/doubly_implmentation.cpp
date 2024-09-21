623f3ae8fcd98183f720328a5cb8effee61a5150
dhruvaa24 <dhruvagrawal247@gmail.com>
2024-09-22 02:21:36 +0530

    Doubly LL implemented

---
+++ linked list/doubly_implmentation.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+
+        Node(int data){
+            this -> data = data;
+            this -> next = NULL;
+            this -> prev = NULL;
+        }
+
+        ~Node(){
+            int value = this -> data;
+            if(this->next!=NULL){
+                delete next;
+                this -> next = NULL;
+            }
+            cout << "Memory cleared with data : " << value << endl;
+        }
+};
+
+void print(Node* & head){
+    Node* temp = head;
+    cout << endl;
+    while(temp!=NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertAtBegin(Node* & head, int data){
+    Node* temp = new Node(data);
+    temp->next = head;
+    head->prev = temp;
+    head=temp;
+}
+
+void insertAtEnd(Node* & tail, int data){
+    Node* temp = new Node(data);
+    temp->prev = tail;
+    tail->next = temp;
+    tail = temp;
+}
+
+void insertAtPos(Node* & head, Node* & tail, int pos, int data){
+    if(pos==1){
+        insertAtBegin(head, data);
+        return;
+    }
+    else{
+        Node* temp = new Node(data);
+        Node* itr = head;
+        Node* curr = head->next;
+        for(int i=1;i<pos-1;i++){
+            itr = itr->next;
+            curr = curr->next;
+        }
+        if(itr->next == NULL||curr==NULL){
+            insertAtEnd(tail, data);
+            return;
+        }
+        else{
+            temp->next = curr;
+            curr->prev = temp;
+            temp->prev = itr;
+            itr->next = temp;
+        }
+    }
+}
+
+int main(){
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    Node* tail = node1;
+    insertAtBegin(head, 12);
+    insertAtBegin(head, 89);
+    insertAtEnd(tail, 23);
+    insertAtEnd(tail, 67);
+    print(head);
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+}
